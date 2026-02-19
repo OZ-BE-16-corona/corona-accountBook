260eeecb4b7f569b717d54ef53690637a186b0a0
SuyeongUeno <flrhsaks77@gmail.com>
2026-02-19 17:44:48 +0900

    mission 3

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -1,4 +1,8 @@
 from .base import *
 
-DEBUG = True
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+DATABASES["default"]["HOST"] = "127.0.0.1"
+DATABASES["default"]["PORT"] = "55432"
+DATABASES["default"]["USER"] = "corona_user"
+DATABASES["default"]["PASSWORD"] = "1234"
+DATABASES["default"]["NAME"] = "corona"
+
