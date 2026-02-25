fe255596972f79c8ad8b343a24b46e80397a8ac6
jun2k5 <70262356+jun2k5@users.noreply.github.com>
2026-02-25 10:57:10 +0900

    Revert "feat: url mapping and admin/user fixes"

--- account/urls.py
+++ account/urls.py
@@ -1,10 +1,4 @@
 from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("<int:account_id>/", views.account_detail),
-    path("count/", views.account_count),
-]
 from .views import AccountListCreateView, AccountDetailDeleteView
 
 app_name = 'account'
--- account/views.py
+++ account/views.py
@@ -1,12 +1,3 @@
-from django.http import JsonResponse
-
-
-def account_detail(request, account_id):
-    return JsonResponse({"account_id": account_id, "message": "계좌 상세 조회"})
-
-
-def account_count(request):
-    return JsonResponse({"total": 3})
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, permissions
