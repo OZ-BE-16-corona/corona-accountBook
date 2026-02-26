52c9fc18f739ed564ecb7fe213031a649123ae35
jun2k5 <jun2k5@naver.com>
2026-02-26 17:18:18 +0900

    fix settings.prod

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,5 +1,13 @@
 from .base import *  # noqa: F403
 
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # 또는 지정하고 싶은 경로
+
 DEBUG = False
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
+
+CELERY_BROKER_URL = "redis://localhost:6379/0"
+CELERY_RESULT_BACKEND = "redis://localhost:6379/0"
+
