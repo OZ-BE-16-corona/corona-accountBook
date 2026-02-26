64d6143024525aade7ad5f38d8214a42a6fcf140
SuyeongUeno <flrhsaks77@gmail.com>
2026-02-26 07:39:14 +0000

    ec2

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -2,4 +2,7 @@ from .base import *  # noqa: F403
 
 DEBUG = False
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
+
+CELERY_BROKER_URL = "redis://localhost:6379/0"
+CELERY_RESULT_BACKEND = "redis://localhost:6379/0"
