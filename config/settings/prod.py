ca7baaa39bf0b9060b68a15c67027c4a6a6c11e6
jun2k5 <jun2k5@naver.com>
2026-02-27 11:13:30 +0900

    test

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -9,3 +9,6 @@ ALLOWED_HOSTS = ["*"]
 
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
+
+CELERY_BROKER_URL = "redis://localhost:6379/0"
+CELERY_RESULT_BACKEND = "redis://localhost:6379/0"
